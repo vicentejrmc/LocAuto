2454cc9cc28f786de3da161beb4fa32416478b9a
VicenteJunior <vicente_pc@outlook.com>
2026-01-11 18:32:46 -0300

    add pasta Compartilhada Dominio

---
+++ server/LocAuto.Dominio/Compartilhado/EntidadeBase.cs
@@ -0,0 +1,13 @@
+﻿namespace LocAuto.Dominio.Compartilhado;
+
+public abstract class EntidadeBase<T>
+{
+    public Guid Id { get; set; }
+
+    public EntidadeBase(Guid id)
+    {
+        Id = Guid.NewGuid();
+    }
+
+    public abstract void AtualizarRegistro(T registroAtualizado);
+}
---
+++ server/LocAuto.Dominio/Compartilhado/IRepositorio.cs
@@ -0,0 +1,11 @@
+﻿    namespace LocAuto.Dominio.Compartilhado;
+
+public interface IRepositorio<T> where T : EntidadeBase<T>
+{
+    // crud - create, read, update, delete
+    public void Inserir(T novoRegistro);
+    public List<T> SelecionarTodos();
+    public T SelecionarPorId(Guid id);
+    public void Editar(Guid id, T registroAtualizado);
+    public void Excluir(Guid id);
+}
